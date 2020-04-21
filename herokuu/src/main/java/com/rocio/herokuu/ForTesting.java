405e966d374d46707c40123e3aef7c8e6f1f2514
Rochsos <46939559+Rochsos@users.noreply.github.com>
2020-04-21 13:58:32 +0200

    Subir Proyecto

---
+++ herokuu/src/main/java/com/rocio/herokuu/ForTesting.java
@@ -0,0 +1,8 @@
+package com.rocio.herokuu;
+
+public class ForTesting {
+
+	public int suma2 (int a, int b) {
+		return a+b;
+	}
+}
---
+++ herokuu/src/test/java/com/rocio/testing/testp/Apptest.java
@@ -0,0 +1,27 @@
+package com.rocio.testing.testp;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.rocio.herokuu.ForTesting;
+
+public class Apptest {
+
+	ForTesting suma = null;
+	
+	@Before
+	public void initialize() {
+		suma = new ForTesting();
+	}
+	
+	@Test
+	public void test() {
+		//fail("Not yet implemented")
+		int expected = 5;
+		assertEquals(expected, suma.suma2(2, 2));
+	}
+	}
+
+
